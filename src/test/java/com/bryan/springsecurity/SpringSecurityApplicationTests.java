59f7aa76c7cc4e7b16f2e3a0acd9bb40643e669b
Bryan Tony <ciprianot.bryan@gmail.com>
2022-10-09 03:44:10 -0500

    Other PasswordEncoders

--- src/test/java/com/bryan/springsecurity/SpringSecurityApplicationTests.java
+++ src/test/java/com/bryan/springsecurity/SpringSecurityApplicationTests.java
@@ -2,12 +2,17 @@ package com.bryan.springsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testPasswordEncoders() {
+        System.out.println(new BCryptPasswordEncoder().encode("password"));
+        System.out.println(new BCryptPasswordEncoder().encode("password"));
+        System.out.println(new Pbkdf2PasswordEncoder().encode("password"));
     }
 
 }
